03361b88156bf06f0fddac7187420965a49265e0
nkleinmann <nekleinmann@gmail.com>
2021-08-01 20:49:57 -0400

    Update 8.2.1 solution

--- rest_rant.sql
+++ rest_rant.sql
@@ -26,34 +26,12 @@ CREATE TABLE public.places
     CONSTRAINT place_pkey PRIMARY KEY (place_id)
 )
 
-TABLESPACE pg_default;
-
-ALTER TABLE public.places
-    OWNER to postgres;
+-- ALTER TABLE public.places
+--     OWNER to postgres;
 
 -- Table: public.comments
 
 -- DROP TABLE public.comments;
-
--- CREATE TABLE public.comments
--- (
---     comment_id integer NOT NULL DEFAULT nextval('comment_comment_id_seq'::regclass),
---     place_id smallint NOT NULL,
---     content character varying COLLATE pg_catalog."default",
---     stars smallint NOT NULL,
---     rant boolean,
---     author character varying COLLATE pg_catalog."default",
---     CONSTRAINT comment_pkey PRIMARY KEY (comment_id),
---     CONSTRAINT comment_place_id_fkey FOREIGN KEY (place_id)
---         REFERENCES public.places (place_id) MATCH SIMPLE
---         ON UPDATE NO ACTION
---         ON DELETE NO ACTION
--- )
-
--- TABLESPACE pg_default;
-
--- ALTER TABLE public.comments
---     OWNER to postgres;
 
 CREATE TABLE comments (
 	comment_id serial NOT NULL,
@@ -65,3 +43,6 @@ CREATE TABLE comments (
 	CONSTRAINT comments_pkey PRIMARY KEY (comment_id),
 	CONSTRAINT comments_places_id_fkey FOREIGN KEY (place_id) REFERENCES places (place_id)
 );
+
+-- ALTER TABLE public.comments
+--     OWNER to postgres;
